2043649e276c4e3cd934885d042ff2264bf13ad7
dawnpioneer <dawnpioneer1989@gmail.com>
2017-09-09 15:19:47 +0800

    stop for create default data

--- grails-app/init/daybook/BootStrap.groovy
+++ grails-app/init/daybook/BootStrap.groovy
@@ -3,32 +3,36 @@ package daybook
 class BootStrap {
 
     def init = { servletContext ->
-        List<DaybookCategory> daybookCategoryList = []
+        def createDefaultData = false
 
-        (1..50).each {
-            def daybookCategory = new DaybookCategory(
-                    name: "test $it",
-                    category: DaybookCategory.Category.EXPENSE
-            )
-            if (it % 3 == 0 ) {
-                daybookCategory.category = DaybookCategory.Category.INCOME
+        if (createDefaultData) {
+            List<DaybookCategory> daybookCategoryList = []
+
+            (1..50).each {
+                def daybookCategory = new DaybookCategory(
+                        name: "test $it",
+                        category: DaybookCategory.Category.EXPENSE
+                )
+                if (it % 3 == 0 ) {
+                    daybookCategory.category = DaybookCategory.Category.INCOME
+                }
+                daybookCategory.save flush: true, failOnError: true
+                daybookCategoryList.add(daybookCategory)
             }
-            daybookCategory.save flush: true, failOnError: true
-            daybookCategoryList.add(daybookCategory)
-        }
 
-        def start = Date.parse('yyyy-MM-dd', '2014-01-01')
-        def end = Date.parse('yyyy-MM-dd', '2017-08-17')
+            def start = Date.parse('yyyy-MM-dd', '2014-01-01')
+            def end = Date.parse('yyyy-MM-dd', '2017-08-17')
 
-        (1..500).each {
-            def daybook = new Daybook(
-                    daybookCategory: daybookCategoryList.get(new Random().nextInt(7)),
-                    title: "daybook $it",
-                    amount: Math.abs(new Random().nextInt() % 20000 + 5000),
-                    recordDate: randomDate(start..end),
-                    comment: "comment $it"
-            )
-            daybook.save flush: true, failOnError: true
+            (1..500).each {
+                def daybook = new Daybook(
+                        daybookCategory: daybookCategoryList.get(new Random().nextInt(7)),
+                        title: "daybook $it",
+                        amount: Math.abs(new Random().nextInt() % 20000 + 5000),
+                        recordDate: randomDate(start..end),
+                        comment: "comment $it"
+                )
+                daybook.save flush: true, failOnError: true
+            }
         }
     }
     def destroy = {
